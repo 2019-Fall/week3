4edf2aacbc31c3b41b183bf3538278c53489d42f
asotoolsny <asotoolsny@gmail.com>
2020-01-26 17:58:34 -0500

    final changes

--- functions.py
+++ functions.py
@@ -28,3 +28,23 @@ def triangle_area(height=0, base=0):
     return height * base * 0.5
 
 print(triangle_area(4, 5))
+
+def describe_city(city, county='usa'):
+    """ return the message with city and country"""
+    return (f'{city.title()} is in {county.upper()}')
+
+print(describe_city("chicago"))
+print(describe_city("paris","france"))
+
+def remove_duplicate(anylist):
+    return set(anylist)
+
+print(remove_duplicate([2,3,7,4,2, 9,7]))
+
+#polindrome
+def is_polindrome(phrase):
+    return phrase == phrase[::-1]
+
+print(is_polindrome('hello'))
+print(is_polindrome('mom'))
+print(is_polindrome('kayak'))
