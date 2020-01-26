d2019fd7af2a0bcc7856060dbcdb94cd0a6ded7c
asotoolsny <asotoolsny@gmail.com>
2020-01-26 13:48:58 -0500

    functions added

--- functions.py
+++ functions.py
@@ -4,5 +4,27 @@ def greet_user():
     """Function to greet the user."""
     print('Hello!')
 
+# passing the arguments to our function
+def greet_user_arg(name, lastname):
+    """Function to greet the user with name."""
+    return (f'Hello, {name} {lastname}!')
+
+
 # calling the funcitons
-greet_user()
+# greet_user()
+# print(greet_user_arg('John', 'Lee')) 
+# msg = greet_user_arg('John', 'Lee')
+# print(msg)
+
+# keyword (default) arguments
+def greet_user_kwd(name, lastname = ''):
+    """Function to greet the user with name."""
+    return (f'Hello, {name} {lastname}!')
+
+print(greet_user_kwd('John'))
+print(greet_user_kwd('John', lastname='Lee'))
+
+def triangle_area(height=0, base=0):
+    return height * base * 0.5
+
+print(triangle_area(4, 5))
